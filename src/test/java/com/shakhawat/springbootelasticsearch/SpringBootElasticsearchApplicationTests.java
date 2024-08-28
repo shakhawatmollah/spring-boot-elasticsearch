39e7aabb8dc090c60ba4d760768d3ab024ea594c
Shakhawat Mollah <sh.sumon99@gmail.com>
2024-08-29 02:33:10 +0600

    remove default test

--- src/test/java/com/shakhawat/springbootelasticsearch/SpringBootElasticsearchApplicationTests.java
+++ src/test/java/com/shakhawat/springbootelasticsearch/SpringBootElasticsearchApplicationTests.java
@@ -5,9 +5,5 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringBootElasticsearchApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
+    
 }
